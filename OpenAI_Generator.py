f6eb7583ab6ff50a97897ea65f252d07dbb1090f
Tae-Woong Yoon <twy@korea.ac.kr>
2023-10-19 23:15:08 +0900

    Update OpenAI_Generator.py

--- OpenAI_Generator.py
+++ OpenAI_Generator.py
@@ -118,13 +118,13 @@ def switch_between_apps():
     st.session_state.pre_audio_bytes = None
 
 
-def autoplay_audio(file_path: str):
+def autoplay_audio(file_path):
     with open(file_path, "rb") as f:
         data = f.read()
         b64 = base64.b64encode(data).decode()
         md = f"""
             <audio controls autoplay style="width: 100%;">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            <source src="data:audio/wav;base64,{b64}" type="audio/wav">
             </audio>
             """
         st.markdown(
