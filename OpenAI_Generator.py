94c5453e244a8bed490ec0e06cf9bf01ddaf07d2
Tae-Woong Yoon <twy@korea.ac.kr>
2023-10-21 17:03:24 +0900

    Update OpenAI_Generator.py

--- OpenAI_Generator.py
+++ OpenAI_Generator.py
@@ -376,7 +376,7 @@ def openai_create():
 
     with st.sidebar:
         st.write("---")
-        st.write("""<small>
+        st.markdown("""<small>
             T.-W. Yoon, Aug. 2023 | [Playground](https://chatgpt-dalle.streamlit.app/) | [Differential equations](https://diff-eqn.streamlit.app/)
             </small>""", unsafe_allow_html=True
         )
