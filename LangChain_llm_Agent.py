0db3666ea8b3d563c1ee0444c9d26f48d3e14c99
Tae-Woong Yoon <twy@korea.ac.kr>
2024-05-06 00:19:38 +0900

    Update LangChain_llm_Agent.py

--- LangChain_llm_Agent.py
+++ LangChain_llm_Agent.py
@@ -1040,7 +1040,10 @@ def create_text_image():
                     date_string = str(current_date)
                     os.environ["LANGCHAIN_PROJECT"] = "llm_agent_" + date_string
                 else:
-                    if is_bing_subscription_key_valid(bing_subscription_key):
+                    if (
+                        is_bing_subscription_key_valid(bing_subscription_key)
+                        and bing_subscription_key
+                    ):
                         os.environ["BING_SUBSCRIPTION_KEY"] = bing_subscription_key
                         st.session_state.bing_subscription_validity = True
                     else:
